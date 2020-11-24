aa18782413c4c6af7c45526a8c199b8636bda561
Joao Augusto <frajolacommel@gmail.com>
2020-11-24 18:58:31 -0300

    concluido

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string email = "";
+            string pass = "";
+
+            while(true){
+                Console.WriteLine("type your email:");
+                    email= Console.ReadLine();
+
+                Console.WriteLine("type your pass :");
+                pass = Console.ReadLine();
+
+                if(email == pass){
+                    Console.WriteLine("you cannot use the same word for email and  password !!!");
+                }else{break;}
+            }
+            Console.WriteLine("Thats okay , welcome " + email);
+        }
+    }
+}
